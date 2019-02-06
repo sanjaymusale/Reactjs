916355db7812194a415b878a9be16fe394e44968
sanjaymusale <sanjaymusale.10@gmail.com>
2019-02-06 11:10:07 +0530

    new react app

---
+++ ticket-master/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello React</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
